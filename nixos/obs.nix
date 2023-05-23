8664a295703a1c4a24f2ae7fe49c55362f890e10
kuviman <kuviman@gmail.com>
2023-05-23 20:38:33 +0400

    Failed attempt to fix OBS

--- nixos/obs.nix
+++ nixos/obs.nix
@@ -1,6 +1,18 @@
 { config, pkgs, ... }:
 
 {
+  nixpkgs.overlays = [
+    (final: prev: {
+      obs-studio = prev.obs-studio.overrideAttrs (oldAttrs: {
+        preFixup = ''
+          qtWrapperArgs+=(
+            --prefix LD_LIBRARY_PATH : "${with final; lib.makeLibraryPath [ xorg.libX11 libvlc libGL ]}"
+            ''${gappsWrapperArgs[@]}
+          )
+        '';
+      });
+    })
+  ];
   environment.systemPackages = with pkgs; [
     (wrapOBS {
       plugins = with pkgs.obs-studio-plugins; [
