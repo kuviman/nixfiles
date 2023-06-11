d124de939be4a152901be8807e34e55bd9b97a30
kuviman <kuviman@gmail.com>
2023-06-11 19:24:24 +0400

    nvim: Auto reload files when modified externally

--- nixos/nvim/my-config/lua/init.lua
+++ nixos/nvim/my-config/lua/init.lua
@@ -404,3 +404,11 @@ require "nvim-treesitter.configs".setup {
     },
   },
 }
+
+-- auto-reload files when modified externally
+-- https://unix.stackexchange.com/a/383044
+vim.o.autoread = true
+vim.api.nvim_create_autocmd({ "BufEnter", "CursorHold", "CursorHoldI", "FocusGained" }, {
+  command = "if mode() != 'c' | checktime | endif",
+  pattern = { "*" },
+})
