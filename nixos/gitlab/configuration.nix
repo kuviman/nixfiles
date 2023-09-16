047a0510ac169171b1c0b21af3ba1de7a4ba3eba
kuviman <kuviman@gmail.com>
2023-09-16 19:06:25 +0400

    gitlab

--- nixos/gitlab/configuration.nix
+++ nixos/gitlab/configuration.nix
@@ -8,15 +8,13 @@
 
   services.gitlab-runner.enable = true;
   services.gitlab-runner.services = {
-
-    # runner for building in docker via host's nix-daemon
-    # nix store will be readable in runner, might be insecure
     nix = {
       # File should contain at least these two variables:
       # `CI_SERVER_URL`
       # `REGISTRATION_TOKEN`
       registrationConfigFile = "/run/secrets/gitlab-runner-registration";
       dockerImage = "alpine";
+
       dockerVolumes = [
         "/nix/store:/nix/store:ro"
         "/nix/var/nix/db:/nix/var/nix/db:ro"
@@ -33,13 +31,12 @@
         mkdir -p -m 1777 /nix/var/nix/profiles/per-user
         mkdir -p -m 0755 /nix/var/nix/profiles/per-user/root
         mkdir -p -m 0700 "$HOME/.nix-defexpr"
-
-        . ${pkgs.nix}/etc/profile.d/nix.sh
-
-        ${pkgs.nix}/bin/nix-env -i ${pkgs.lib.concatStringsSep " " (with pkgs; [ nix cacert git openssh ])}
-
-        ${pkgs.nix}/bin/nix-channel --add https://nixos.org/channels/nixpkgs-unstable
+        . ${pkgs.nix}/etc/profile.d/nix-daemon.sh
+        ${pkgs.nix}/bin/nix-channel --add https://nixos.org/channels/nixos-20.09 nixpkgs # 3
         ${pkgs.nix}/bin/nix-channel --update nixpkgs
+        ${pkgs.nix}/bin/nix-env -i ${pkgs.lib.concatStringsSep " " (with pkgs; [ nix cacert git openssh ])}
+        mkdir -p ~/.config/nix
+        echo "experimental-features = nix-command flakes" > ~/.config/nix/nix.conf
       '';
       environmentVariables = {
         ENV = "/etc/profile";
