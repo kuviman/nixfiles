21abfaf9136a3f26f061fb817c045c39bd4a3313
kuviman <kuviman@gmail.com>
2023-11-25 12:56:38 +0400

    back to nixos-unstable

--- flake.nix
+++ flake.nix
@@ -3,7 +3,7 @@
 
   inputs = {
     # Nixpkgs
-    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     # Secrets
     sops.url = "github:Mic92/sops-nix";
     nur.url = "github:nix-community/NUR";
