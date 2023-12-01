963f2064c48119c83b1784c6012f8724d615264f
kuviman <kuviman@gmail.com>
2023-12-01 16:18:07 +0400

    xone?

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -151,5 +151,5 @@
   # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
   system.stateVersion = "22.11"; # Did you read the comment?
 
-  hardware.xpadneo.enable = true;
+  hardware.xone.enable = true;
 }
