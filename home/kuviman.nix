1524be482bb3ad61af7c220bccbc6a1112406de7
kuviman <kuviman@gmail.com>
2023-12-08 21:29:32 +0400

    ttv only on mainix

--- home/kuviman.nix
+++ home/kuviman.nix
@@ -1,15 +1,34 @@
-{ config, pkgs, inputs, system, ... }: {
+{ config, hostname, pkgs, inputs, system, ... }: {
   email = "kuviman@gmail.com";
 
-  systemd.user.services = {
-    ttv =
-      let
-        ttv-bin = inputs.ttv.defaultApp.${system};
-        workDir = config.home.homeDirectory + "/.ttv";
-      in
-      {
+  systemd.user.services =
+    pkgs.lib.mkIf (hostname == "mainix") {
+      ttv =
+        let
+          ttv-bin = inputs.ttv.defaultApp.${system};
+          workDir = config.home.homeDirectory + "/.ttv";
+        in
+        {
+          Unit = {
+            Description = "Twitch Stuff";
+            After = [ "network.target" ];
+          };
+
+          Install = {
+            WantedBy = [ "default.target" ];
+          };
+
+          Service = {
+            Type = "simple";
+            WorkingDirectory = workDir;
+            ExecStart = "${ttv-bin.program} --server 0.0.0.0:1789";
+            Restart = "always";
+            RestartSec = "10";
+          };
+        };
+      ttv-web = {
         Unit = {
-          Description = "Twitch Stuff";
+          Description = "Twitch Stuff (Frontend serving)";
           After = [ "network.target" ];
         };
 
@@ -17,39 +36,21 @@
           WantedBy = [ "default.target" ];
         };
 
-        Service = {
-          Type = "simple";
-          WorkingDirectory = workDir;
-          ExecStart = "${ttv-bin.program} --server 0.0.0.0:1789";
-          Restart = "always";
-          RestartSec = "10";
-        };
+        Service =
+          let
+            caddyfile = pkgs.writeText "Caddyfile" ''
+              :1787
+              header Cache-Control no-store
+              root * ${inputs.ttv.web}
+              file_server
+            '';
+          in
+          {
+            Type = "simple";
+            ExecStart = "${pkgs.caddy}/bin/caddy run --config ${caddyfile} --adapter caddyfile";
+            Restart = "always";
+            RestartSec = "10";
+          };
       };
-    ttv-web = {
-      Unit = {
-        Description = "Twitch Stuff (Frontend serving)";
-        After = [ "network.target" ];
-      };
-
-      Install = {
-        WantedBy = [ "default.target" ];
-      };
-
-      Service =
-        let
-          caddyfile = pkgs.writeText "Caddyfile" ''
-            :1787
-            header Cache-Control no-store
-            root * ${inputs.ttv.web}
-            file_server
-          '';
-        in
-        {
-          Type = "simple";
-          ExecStart = "${pkgs.caddy}/bin/caddy run --config ${caddyfile} --adapter caddyfile";
-          Restart = "always";
-          RestartSec = "10";
-        };
     };
-  };
 }
