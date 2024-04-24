a7ffa8c45a155efa77dc0555789290b2c0b50753
kuviman <kuviman@gmail.com>
2024-04-24 18:02:59 +0400

    fix warning

--- nixos/kde.nix
+++ nixos/kde.nix
@@ -2,7 +2,7 @@
 
 {
   services.xserver.enable = true;
-  services.xserver.desktopManager.plasma6.enable = true;
+  services.desktopManager.plasma6.enable = true;
   environment.plasma6.excludePackages = with pkgs.kdePackages; [
   ];
   services.xserver.desktopManager.gnome.enable = true;
