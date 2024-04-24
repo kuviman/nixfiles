6f952fad145eb2cc0946c79ab71b4b9412159433
kuviman <kuviman@gmail.com>
2024-04-24 18:01:50 +0400

    kde & gnome ssh workaround

--- nixos/kde.nix
+++ nixos/kde.nix
@@ -6,4 +6,7 @@
   environment.plasma6.excludePackages = with pkgs.kdePackages; [
   ];
   services.xserver.desktopManager.gnome.enable = true;
+
+  # https://github.com/NixOS/nixpkgs/issues/75867
+  programs.ssh.askPassword = pkgs.lib.mkForce "${pkgs.ksshaskpass.out}/bin/ksshaskpass";
 }
