02018c48a3be4d316f0d691e3e878ac510efd47b
kuviman <kuviman@gmail.com>
2024-05-16 10:07:44 +0400

    Disable hyprland by default

--- nixos/hyprland.nix
+++ nixos/hyprland.nix
@@ -1,23 +1,32 @@
-{ pkgs, inputs, ... }:
+{ pkgs, lib, config, inputs, ... }:
 
 {
-  nixpkgs.overlays = [
-    (self: super: {
-      hyprland = inputs.hyprland.packages.${self.system}.hyprland;
-    })
-  ];
-  programs.hyprland = {
-    enable = true;
-    package = inputs.hyprland.packages.${pkgs.system}.hyprland;
-    xwayland.enable = true;
+  options = {
+    kuviman.hyprland.enable = lib.mkOption
+      {
+        type = lib.types.bool;
+        default = false;
+      };
   };
-  environment.systemPackages = with pkgs; [
-    waybar
-    hyprpaper
-    hyprpicker
-    hyprshot
-    mako # notifications
-    wofi
-    pavucontrol
-  ];
+  config = lib.mkIf config.kuviman.hyprland.enable {
+    nixpkgs.overlays = [
+      (self: super: {
+        hyprland = inputs.hyprland.packages.${self.system}.hyprland;
+      })
+    ];
+    programs.hyprland = {
+      enable = true;
+      package = inputs.hyprland.packages.${pkgs.system}.hyprland;
+      xwayland.enable = true;
+    };
+    environment.systemPackages = with pkgs; [
+      waybar
+      hyprpaper
+      hyprpicker
+      hyprshot
+      mako # notifications
+      wofi
+      pavucontrol
+    ];
+  };
 }
