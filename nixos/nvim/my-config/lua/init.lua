979f5bcb90bea826b12717fcc058cfc1870771ee
kuviman <kuviman@gmail.com>
2024-09-16 09:04:53 +0400

    nvim format on save

--- nixos/nvim/my-config/lua/init.lua
+++ nixos/nvim/my-config/lua/init.lua
@@ -453,3 +453,19 @@ require("ibl").setup({
   },
   scope = { enabled = true },
 })
+
+-- format on save (https://www.mitchellhanberg.com/modern-format-on-save-in-neovim/)
+vim.api.nvim_create_autocmd("LspAttach", {
+  group = vim.api.nvim_create_augroup("lsp", { clear = true }),
+  callback = function(args)
+    -- 2
+    vim.api.nvim_create_autocmd("BufWritePre", {
+      -- 3
+      buffer = args.buf,
+      callback = function()
+        -- 4 + 5
+        vim.lsp.buf.format {async = false, id = args.data.client_id }
+      end,
+    })
+  end
+})
