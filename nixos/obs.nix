fbc782390bcc7ea822185734c8936ac9d41ecb1f
kuviman <kuviman@gmail.com>
2025-06-13 21:33:45 +0400

    bring back droidcam

--- nixos/obs.nix
+++ nixos/obs.nix
@@ -5,7 +5,7 @@
     (wrapOBS {
       plugins = with obs-studio-plugins; [
         # wlrobs
-        # droidcam-obs
+        droidcam-obs
         input-overlay
         # obs-backgroundremoval
         obs-vkcapture
