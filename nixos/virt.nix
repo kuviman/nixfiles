7fea6f8de5c26e71873608a7a071ed14650cbcbe
kuviman <kuviman@gmail.com>
2025-11-09 04:05:04 +0400

    virt-viewer

--- nixos/virt.nix
+++ nixos/virt.nix
@@ -1,6 +1,9 @@
-{ config, ... }:
+{ config, pkgs, ... }:
 {
   virtualisation.libvirtd.enable = true;
   programs.virt-manager.enable = true;
   users.users.${config.nixfiles.username}.extraGroups = [ "libvirtd" ];
+  environment.systemPackages = with pkgs;[
+    virt-viewer
+  ];
 }
