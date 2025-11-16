d3d797c13c2da7733ea6c6e1888b49d2f2f846a4
kuviman <kuviman@gmail.com>
2025-11-16 12:50:23 +0400

    configure niri in hm

--- home/niri/default.nix
+++ home/niri/default.nix
@@ -5,6 +5,22 @@
     inputs.dankMaterialShell.homeModules.dankMaterialShell.default
     inputs.dankMaterialShell.homeModules.dankMaterialShell.niri
   ];
+  programs.niri = {
+    package = pkgs-unstable.niri;
+    settings = {
+      input.mouse = {
+        accel-speed = -0.2;
+        accel-profile = "flat";
+      };
+      outputs = {
+        "DP-2".position = { x = 0; y = 0; };
+        "HDMI-A-1".position = {
+          x = 2560;
+          y = 180;
+        };
+      };
+    };
+  };
   programs.dankMaterialShell = {
     enable = true;
     niri = {
