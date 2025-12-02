3ca1eccdbf65b55e3a34e1cc7ee67d1e7f5eb135
kuviman <kuviman@gmail.com>
2025-12-03 03:55:28 +0400

    lxd was removed

--- nixos/lxc.nix
+++ nixos/lxc.nix
@@ -1,7 +1,5 @@
-{ config, ... }:
+{ ... }:
 {
   virtualisation.lxc.enable = true;
   virtualisation.lxc.lxcfs.enable = true;
-  virtualisation.lxd.enable = true;
-  users.users.${config.nixfiles.username}.extraGroups = [ "lxd" ];
 }
