bee3bffe9771dbc6110154f2f43ef08bd3c614c4
kuviman <kuviman@gmail.com>
2025-12-03 04:00:47 +0400

    android-udev-tools no longer necessary

--- nixos/android.nix
+++ nixos/android.nix
@@ -4,7 +4,6 @@
   programs.adb.enable = true;
   users.users.${config.nixfiles.username}.extraGroups = [ "adbusers" ];
   environment.systemPackages = with pkgs; [
-    android-udev-rules
     scrcpy
     jmtpfs
   ];
