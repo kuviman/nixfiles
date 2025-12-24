6527b42a5b4753a9e901671f7a0e3f53d64add18
kuviman <kuviman@gmail.com>
2025-12-24 10:16:14 +0400

    Use tokei with kast support

--- nixos/packages.nix
+++ nixos/packages.nix
@@ -1,5 +1,68 @@
 { pkgs, inputs, system, ... }:
 
+let
+  tokei = with pkgs;
+    rustPlatform.buildRustPackage (finalAttrs: {
+      pname = "tokei";
+      version = "13.0.0";
+
+      src = fetchFromGitHub {
+        owner = "kuviman";
+        repo = "tokei";
+        rev = "c19576322a7fdfe7c85d50b4041e7057805da9fc";
+        hash = "sha256-/TSSuqP5+JmDHUHzJ8IHBIMTkKZovMOVgbEmBUnSUd4=";
+      };
+
+      # patches = [
+      #   (fetchpatch2 {
+      #     # https://github.com/XAMPPRocky/tokei/pull/1209
+      #     url = "https://github.com/XAMPPRocky/tokei/commit/ce8d8535276a2e41878981a8199232986ab96c6b.patch";
+      #     hash = "sha256-1tb+WmjVsTxs8Awf1mbKOBIhJ3ddoOT8ZjBKA2BMocg=";
+      #   })
+      # ];
+
+      cargoHash = "sha256-R9PI85ay7DoOTM4iwAv24X70W4vU0Y2gg+lQtaTLj/s=";
+
+      buildInputs = lib.optionals stdenv.hostPlatform.isDarwin [ libiconv ];
+
+      checkInputs = lib.optionals stdenv.hostPlatform.isDarwin [ zlib ];
+
+      # enable all output formats
+      buildFeatures = [ "all" ];
+
+      nativeInstallCheckInputs = [ versionCheckHook ];
+      versionCheckProgramArg = "--version";
+      doInstallCheck = true;
+
+      passthru.updateScript = nix-update-script { };
+
+      meta = {
+        description = "Count your code, quickly";
+        longDescription = ''
+          Tokei is a program that displays statistics about your code. Tokei will show the number of files, total lines within those files and code, comments, and blanks grouped by language.
+        '';
+        homepage = "https://github.com/XAMPPRocky/tokei";
+        license = with lib.licenses; [
+          asl20 # or
+          mit
+        ];
+        maintainers = with lib.maintainers; [ defelo ];
+        mainProgram = "tokei";
+      };
+    });
+  tokei_not_working = (pkgs.tokei.override
+    {
+      src = pkgs.fetchFromGitHub {
+        owner = "kuviman";
+        repo = "tokei";
+        rev = "c19576322a7fdfe7c85d50b4041e7057805da9fc";
+        hash = "sha256-/TSSuqP5+JmDHUHzJ8IHBIMTkKZovMOVgbEmBUnSUd4=";
+      };
+      cargoHash = "";
+      cargoSha256 = "";
+    });
+in
+
 {
   programs.steam.enable = true;
 
