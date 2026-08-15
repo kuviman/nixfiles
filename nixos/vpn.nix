1bb005e5649fb716da692d1b2b88f973cba1aa20
kuviman <kuviman@gmail.com>
2026-08-15 22:26:11 +0400

    wireproxy-awg

--- nixos/vpn.nix
+++ nixos/vpn.nix
@@ -1,5 +1,19 @@
 { pkgs, ... }:
 
+let
+  wireproxy-awg =
+    pkgs.wireproxy.overrideAttrs (finalAttrs: {
+      version = "1.0.17";
+      src = pkgs.fetchFromGitHub {
+        owner = "artem-russkikh";
+        repo = "wireproxy-awg";
+        rev = "v${finalAttrs.version}";
+        hash = "sha256-nxzgEg8SKkrYXOLu4ikCkmaCZB6pYHdSTYAiWmv0TfM=";
+      };
+      vendorHash = "sha256-95Gjh5bD3TGLgAALLyVV27zmxTGSqGK7bvYS9UaTGmE=";
+    });
+in
+
 {
   # for l2tp vpn - https://github.com/NixOS/nixpkgs/issues/64965#issuecomment-741920446
   services.strongswan = {
@@ -11,6 +25,7 @@
   programs.amnezia-vpn = {
     enable = true;
   };
+  environment.systemPackages = [ wireproxy-awg ];
   services.v2raya = {
     enable = true;
     cliPackage = pkgs.xray;
